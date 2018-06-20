ca611c879d3d7d2450a6d1a0558eba4d1b4ed278
root <root@localhost.localdomain>
2018-06-20 09:37:05 +0800

    初始化项目版本

---
+++ init.lua
@@ -0,0 +1,13 @@
+packagename="com.ixinye.xyblog"
+template="tab"
+user_permission={
+  [1]	= "ACCESS_COARSE_LOCATION" ;
+  [2]	= "ACCESS_FINE_LOCATION" ;
+  [3]	= "ACCESS_NETWORK_STATE" ;
+  [4]	= "ACCESS_WIFI_STATE" ;
+  [5]	= "INTERNET" ;
+  [6]	= "WRITE_EXTERNAL_STORAGE" ;
+  } ;
+appcode="20180620"
+appname="心叶博客"
+appver="1.0"
---
+++ sub/1522108493/init.lua
@@ -0,0 +1,2 @@
+template="tool"
+name="浏览文章"
